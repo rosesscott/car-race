86bc24ecf33bcb225248c81b2c5232ef9aa5ef6f
rosesscott <54520029+rosesscott@users.noreply.github.com>
2020-07-02 00:12:27 +1200

    Initial commit

---
+++ holding.py
@@ -0,0 +1,43 @@
+amount_initial = int(input ("Choose a car number... (1-12)? "))
+
+if 1<= amount_initial <=12:
+    print("great!")
+else:
+    print('You need a number between 1 and 12. Try again!')
+
+
+#continue and ask how many rounds they would like to play
+race_distance = int(input ("Choose a race distance... (10-20)? "))
+if 1<= amount_initial <=12:
+    print("great!")
+else:
+    print('You need a number between 1 and 12. Try again!')
+#print("What distance would you like to race? (10-100)")
+
+
+#carlist ['car1', 'car2', 'car3', 'car4' 'car5' 'car6' 'car7' 'car8' 'car9' 'car10' 'car11' 'car12']
+#for car in carlist:
+    #print(car)
+
+
+#ask da players to choose a distance
+#keep track of distances
+#initially distance_travelled = [0,0,0,0,0,0]
+distances_travelled = [1,2,4,0,0,6]
+distances_travelled = [4,2,5,4,2,10]
+
+
+def print_snapshot(mylist):
+    counter=1
+    for car_distance in mylist:
+        print ('car {} has moved \n'. format (counter) +'*'*car_distance)
+        counter+=1
+
+        if car_distance == 10:
+            print(mylist[mylist.index(cardistance)])
+        #pass
+
+
+# call
+#print_snapshot(distance_travelled)
+
---
+++ userchoice.py
@@ -0,0 +1,47 @@
+#create an introduction and ask what car number the user would like to choose.
+
+print("Welcome to my car game! :) simp")
+print("_______________________")
+
+difficult_car = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+
+level = input('which level would you like to play, easy or difficult?')
+print('you have chosen: {}'. format(level))
+
+def carcheck (question, min, max):
+    #def = define
+    # intcheck is the name of my fucntion in python
+    # question - will be the question asked to the user
+    # low and high boundaries will be set up by the progarmmer
+    valid = False
+    while not valid:
+        try: #try to convet input to interger
+            initial_car = int(input(question))
+            if min<= initial_car<=max:
+                print('You chose car', initial_car)
+                # I am happy with the loop
+                break
+            else: #the number is out of bounds
+                print("Please enter a valid car number\nsimp! \nBetween 1 and 12")
+                continue
+        except ValueError:
+            print("Please enter a valid number\nPeasant! This must be a number, please try again")
+#run function - call
+carcheck("Choose a car number... (1-12)? ", 1 , 12)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
